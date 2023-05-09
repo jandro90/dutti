d38bbaf9d51ec932e1800b25f6d6b08d5ab4bab1
Alejandro <agp901@gmail.com>
2023-05-09 15:37:13 +0200

    feat(Logout): added redirection to loginPage when the user click in logout

--- src/app/private/views/principal/principal.component.ts
+++ src/app/private/views/principal/principal.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { User } from 'src/app/interfaces/user';
 import { LoginService } from 'src/app/services/login/login.service';
 
@@ -10,7 +11,7 @@ import { LoginService } from 'src/app/services/login/login.service';
 export class PrincipalComponent implements OnInit {
   currentUser: User;
 
-  constructor(private loginService: LoginService) { }
+  constructor(private loginService: LoginService, private router: Router) { }
 
   ngOnInit(): void {
     this.currentUser = this.loginService.currentUser;
@@ -18,6 +19,7 @@ export class PrincipalComponent implements OnInit {
 
   logout() {
     this.loginService.logout();
+    this.router.navigate(['']);
   }
 
 }

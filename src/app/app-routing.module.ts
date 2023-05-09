4743fc0a6ce8fafe046b5ef811d7d7983758a68e
Alejandro <agp901@gmail.com>
2023-05-09 11:53:33 +0200

    feat(refactor): remove unncessary comments in app-routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-// Components
 
 const routes: Routes = [
   { path: '', loadChildren: () => import(`./public/login.module`).then(m => m.LoginModule)},
   { path: 'principal', loadChildren: () => import(`./private/principal.module`).then(m => m.PrincipalModule) }
-  // { path: 'ships', loadChildren: () => import(`./components/ships/ships.module`).then(m => m.ShipsModule) }
 ];
 
 @NgModule({
